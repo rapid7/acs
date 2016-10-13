8e6a10a27cd42bea48e64a69fe80815556c78105
Alan Sebastian <asebastian2@gmail.com>
2016-10-13 10:46:42 -0700

    Add first test

--- test/acs.js
+++ test/acs.js
@@ -1,9 +1,16 @@
 'use strict';
 
+var request = require('supertest');
+var express = require('express');
 var should = require('chai').should();
+var app = express();
 
-describe('acs', function() {
-  it('equals one', function() {
-    (1).should.equal(1);
+describe('POST encrypt/get_ct_json', function() {
+  it('responds with a json error', function() {
+    request(app)
+      .post('/encrypt/get_ct_json')
+      .expect(200, {
+        error: 'Error: connect ECONNREFUSED 127.0.0.1:4500'
+      });
   });
 });
