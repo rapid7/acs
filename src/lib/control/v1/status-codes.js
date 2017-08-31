e1ce33f915c38d3f4cdf2b4aae2c13eb9ba9eb22
davepgreene <davepgreene@gmail.com>
2017-08-31 17:56:44 -0400

    Fixed issue with status-codes module not setting properties correctly.

--- src/lib/control/v1/status-codes.js
+++ src/lib/control/v1/status-codes.js
@@ -1,17 +1,17 @@
 import http from 'http';
 
-export const STATUS_CODES = Object.assign({}, http.STATUS_CODES);
-
 const M_REJECT_CHARS = /\W/g;
 const M_DELIMITERS = /\s/g;
 
 // Reverse-map status names to codes
-Object.keys(STATUS_CODES).forEach(function _(code) {
-  const message = STATUS_CODES[code];
+export default Object.keys(http.STATUS_CODES).reduce((accumulator, code) => {
+  const message = http.STATUS_CODES[code];
   let name = message.replace(M_DELIMITERS, '_');
 
   name = name.replace(M_REJECT_CHARS, '');
   name = name.toUpperCase();
 
-  STATUS_CODES[name] = Number(code);
-});
+  accumulator[name] = Number(code);
+
+  return accumulator;
+}, {});
