c95d2747484dedc239bef1561e8f3d719b0699d5
davepgreene <davepgreene@gmail.com>
2017-09-05 17:04:18 -0400

    Added better error handling for JSON.parse errors. We have handling around our own JSON parsing so this is more to handle errors that body-parser throws.

--- src/lib/control/v1/error.js
+++ src/lib/control/v1/error.js
@@ -12,5 +12,11 @@ export default (err, req, res, next) => { // eslint-disable-line no-unused-vars
   Log.log('ERROR', err);
   const status = err.statusCode || STATUS_CODES.BAD_REQUEST;
 
+  if (err instanceof SyntaxError) {
+    // If we hit a syntax error it's from deserializing JSON and we should do
+    // our best to obfuscate the contents of the error.
+    err.message = 'There was an unexpected error';
+  }
+
   res.status(status).json(err.message);
 };
